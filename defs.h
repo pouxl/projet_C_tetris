4ae8211935cd183290dae7503a3d9622b12b6b8b
MARAIANI08 <matthieu.mariani@telecomnancy.net>
2015-05-26 00:11:39 +0200

    le dernier commit

--- defs.h
+++ defs.h
@@ -9,9 +9,9 @@
 
  
 /* On inclut les libs supplémentaires */
-#include <SDL2_image/SDL_image.h>
-#include <SDL2_ttf/SDL_ttf.h>
-#include <SDL2_mixer/SDL_mixer.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
+#include <SDL2/SDL_mixer.h>
  
 // Taille de la fenêtre : 800x480 pixels 
 #define SCREEN_WIDTH 800
